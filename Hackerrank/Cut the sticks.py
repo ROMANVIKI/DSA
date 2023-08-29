2e5b7f6ed829491261b6049150f7ac1469a3b9dc
ROMANVIKI <vikiadhi20162gmail.com>
2023-08-29 22:19:00 +0530

    aug-29_2

--- Hackerrank/Cut the sticks.py
+++ Hackerrank/Cut the sticks.py
@@ -1,31 +1,28 @@
-#!/bin/python3
+def cutTheSticks(arr, arr2, n):
+    result = []
 
-import math
-import os
-import random
-import re
-import sys
+    while arr:
 
-#
-# Complete the 'cutTheSticks' function below.
-#
-# The function is expected to return an INTEGER_ARRAY.
-# The function accepts INTEGER_ARRAY arr as parameter.
-#
+        result.append(len(arr))
 
-def cutTheSticks(arr):
-    # Write your code here
+        min_length = min(arr)
 
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+        arr = [x - min_length for x in arr if x - min_length > 0]
+    
+    return result
+if __name__ == '__main__':  
+    # fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
     n = int(input().strip())
 
     arr = list(map(int, input().rstrip().split()))
 
-    result = cutTheSticks(arr)
+    arr2 = [0]
 
-    fptr.write('\n'.join(map(str, result)))
-    fptr.write('\n')
+    result = cutTheSticks(arr, arr2, n)
 
-    fptr.close()
+    print(result)
+    # fptr.write('\n'.join(map(str, result)))
+    # fptr.write('\n')
+
+    # fptr.close()
