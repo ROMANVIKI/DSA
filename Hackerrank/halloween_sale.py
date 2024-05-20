ac3a91665cd20521f537726729e5381bd4bcc729
ROMANVIKI <vikiadhi2016@gmail.com>
2024-05-20 23:37:47 +0530

    halloween sale problem

---
+++ Hackerrank/halloween_sale.py
@@ -0,0 +1,37 @@
+# p = 20
+# d = 3
+# m = 6
+# s = 80
+
+def howManyGames(p, d, m, s):
+    count = 0
+    while s >= p:
+        s-=p
+        count += 1
+        p = max(p - d, m)
+    print(count)
+
+
+if __name__ == '__main__':
+    # fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    p = int(first_multiple_input[0])
+
+    d = int(first_multiple_input[1])
+
+    m = int(first_multiple_input[2])
+
+    s = int(first_multiple_input[3])
+
+    answer = howManyGames(p, d, m, s)
+
+    # fptr.write(str(answer) + '\n')
+
+    # fptr.close()
+
+# p - intial price
+# d - dollars less than prev price
+# m - less than or equal to this value 
+# s - budget
