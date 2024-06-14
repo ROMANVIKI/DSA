dfd421e16d42f3146f128fc6239048806b724c7f
ROMANVIKI <vikiadhi2016@gmail.com>
2024-06-14 16:44:08 +0530

    minimum-distance problem hr

---
+++ Hackerrank/minimum-distance.py
@@ -0,0 +1,39 @@
+
+
+def minimumDistances(a):
+    same_num_indices = []
+    # Write your code here
+    for i in range(len(a)):
+        for j in range(len(a)):
+            if i == j:
+                pass
+            else:
+                if  a[i] == a[j]:
+                    min_value = i - j
+                    if min_value > 0:
+                       
+                        same_num_indices.append(min_value)
+    if not same_num_indices:
+        min_dist = -1
+    else:
+        min_dist = min(same_num_indices)
+    return min_dist
+    # return same_num_indices
+
+
+
+
+if __name__ == '__main__':
+    # fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = minimumDistances(a)
+
+    print(result)
+
+    # fptr.write(str(result) + '\n')
+
+    # fptr.close()
