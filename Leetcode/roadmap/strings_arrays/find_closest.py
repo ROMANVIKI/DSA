59549df447ed5e14eba11bb6a40bc888ed1b269d
ROMANVIKI <vikiadhi2016@gmail.com>
2024-09-20 10:08:14 +0530

    roadmap.io

---
+++ Leetcode/roadmap/strings_arrays/find_closest.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findClosestNumber(self, nums: List[int]):
+        closest = nums[0]
+        for num in nums:
+            if abs(num) < abs(closest) or (abs(num) == abs(closest) and num > closest):
+                closest = num
+                
+        return closest
+
+
+
+
+sol = Solution()
+print(sol.findClosestNumber)
+
